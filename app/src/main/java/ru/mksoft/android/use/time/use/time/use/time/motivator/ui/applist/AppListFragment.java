88edb3be2d31178e830264fa70d33a2d2cb49932
kirill <kirillmaglys@gmail.com>
2022-01-07 19:21:11 +0300

    Написал основу

--- app/src/main/java/ru/mksoft/android/use/time/use/time/use/time/motivator/ui/applist/AppListFragment.java
+++ app/src/main/java/ru/mksoft/android/use/time/use/time/use/time/motivator/ui/applist/AppListFragment.java
@@ -3,21 +3,19 @@ package ru.mksoft.android.use.time.use.time.use.time.motivator.ui.applist;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import ru.mksoft.android.use.time.use.time.use.time.motivator.R;
 import ru.mksoft.android.use.time.use.time.use.time.motivator.databinding.FragmentAppListBinding;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class AppListFragment extends Fragment {
@@ -31,29 +29,42 @@ public class AppListFragment extends Fragment {
                 new ViewModelProvider(this).get(AppListViewModel.class);
 
         binding = FragmentAppListBinding.inflate(inflater, container, false);
-//        View root = binding.getRoot();
 
-        PackageManager pm = getContext().getPackageManager();
+        long startReading = Calendar.getInstance().getTimeInMillis();
+        PackageManager packageManager = getContext().getPackageManager();
         ArrayList<ApplicationInfo> appList = new ArrayList<>();
-        for (ApplicationInfo info : pm.getInstalledApplications(PackageManager.GET_META_DATA)) {
+        List<ApplicationInfo> notSortedAppList = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        int cycleNum = 0;
+        for (ApplicationInfo info : notSortedAppList) {
+            cycleNum++;
             try {
-                if (pm.getLaunchIntentForPackage(info.packageName) != null) {
+                if (packageManager.getLaunchIntentForPackage(info.packageName) != null) {
                     appList.add(info);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+        Log.d("CycleNum", String.valueOf(cycleNum));
+
+        long endReading = Calendar.getInstance().getTimeInMillis();
 
         List<AppCardInfo> appCards = new ArrayList<>(appList.size());
+        //Todo передать appInfo
         for (ApplicationInfo applicationInfo : appList) {
-            appCards.add(new AppCardInfo((String) applicationInfo.loadLabel(pm), applicationInfo.loadIcon(pm), "ABCABA"));
+            appCards.add(new AppCardInfo((String) applicationInfo.loadLabel(packageManager), applicationInfo.loadIcon(packageManager), "ABCABA"));
         }
 
 
+        long startWriting = Calendar.getInstance().getTimeInMillis();
         RecyclerView recyclerView = binding.appListRecyclerView;
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(new CustomRecyclerAdapter(appCards));
+        long endWriting = Calendar.getInstance().getTimeInMillis();
+
+        Log.d("ReadTime", String.valueOf(endReading - startReading));
+        Log.d("WriteTime", String.valueOf(endWriting - startWriting));
 
         return binding.getRoot();
     }
